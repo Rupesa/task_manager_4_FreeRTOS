1a923ff0e8251affb7cfbf003af58be173a75ea5
Rodrigo Santos <56035598+RodrigoSantosgit@users.noreply.github.com>
2022-01-28 19:27:24 +0000

    STEP1 DONE

--- PIC32MX_ChipKitLedBlink/mainSETRLedBlink.c
+++ PIC32MX_ChipKitLedBlink/mainSETRLedBlink.c
@@ -31,28 +31,24 @@
 #include "../UART/uart.h"
 
 /* Set the tasks' period (in system ticks) */
-int TASK_A_PERIOD = 100;
-int TASK_B_PERIOD = 50;
-int TASK_C_PERIOD = 20;
-#define TASK_A_PERIOD_MS 	( 3000 / portTICK_RATE_MS )
-#define TASK_B_PERIOD_MS 	( 2000 / portTICK_RATE_MS )
-#define TASK_C_PERIOD_MS 	( 1000 / portTICK_RATE_MS )
+
 #define TASK_TICK_PERIOD_MS 	( 100 / portTICK_RATE_MS )
 
+int TASK_TICK_PERIOD;
 int TMAN_TICK;      // TASK MANAGER TICK COUNTER
+
 /* Control the load task execution time (# of iterations)*/
 /* Each unit corresponds to approx 50 ms*/
 #define INTERF_WORKLOAD          ( 20)
 
 /* Priorities of the demo application tasks (high numb. -> high prio.) */
 #define TASK_A_PRIORITY	( tskIDLE_PRIORITY + 1 )
-#define TASK_B_PRIORITY	    ( tskIDLE_PRIORITY + 1 )
+#define TASK_B_PRIORITY	 ( tskIDLE_PRIORITY + 1 )
 #define TASK_C_PRIORITY  ( tskIDLE_PRIORITY + 1 )
+#define TASK_D_PRIORITY  ( tskIDLE_PRIORITY + 1 )
+#define TASK_E_PRIORITY  ( tskIDLE_PRIORITY + 1 )
+#define TASK_F_PRIORITY  ( tskIDLE_PRIORITY + 1 )
 #define TASK_TICK_PRIORITY ( tskIDLE_PRIORITY + 2 )
-
-TaskHandle_t xHandleA;
-TaskHandle_t xHandleB;
-TaskHandle_t xHandleC;
 
 /* Task Structure */
 struct TASK {
@@ -60,6 +56,8 @@ struct TASK {
    int period;
    char name;
    int priority;
+   int tick_counter;
+   TaskHandle_t handler;
 };
 
 struct TASK TASKS[6];
@@ -77,29 +75,25 @@ void TMAN_TaskStats(void);
 void task_tick_work(void *pvParam);
 
 
-void TMAN_Init(void)
+void TMAN_Init(int TMAN_TICK_PERIOD_VALUE)
 {
 
     /* Welcome message*/
     printf("**********************************************\n\r");
-    printf("  TMAN - Task Manager framework for FreeRTOS \n\r");
+    printf("  TMAN - Task Manager framework for FreeRTOS  \n\r");
     printf("**********************************************\n\r");
     
     /* Inicialização do Tick */
     TMAN_TICK = 0;
+    TASK_TICK_PERIOD = TMAN_TICK_PERIOD_VALUE;
     
     /* Inicialização da tabela de Tasks */
     malloc(sizeof TASKS);
-    /*TASKS[0].period = TASK_A_PERIOD_MS;
-    TASKS[1].period = TASK_B_PERIOD_MS;    
-    TASKS[2].period = TASK_C_PERIOD_MS;     */
-    TASKS[0].period = TASK_A_PERIOD_MS;
-    TASKS[1].period = TASK_B_PERIOD_MS;    
-    TASKS[2].period = TASK_C_PERIOD_MS;
+
     /* Tick Start */
     xTaskCreate( task_tick_work, ( const signed char * const ) "TICK_TASK", configMINIMAL_STACK_SIZE, NULL, TASK_TICK_PRIORITY, NULL );
     
-    //TMAN_Close();
+
 }
 
 void TMAN_Close(void)
@@ -115,57 +109,79 @@ void TMAN_TaskAdd(char name)
     if (name == 'A'){
         printf("CREATING A \n\r");
         TASKS[0].name = 'A';
-        xTaskCreate( task_work, ( const signed char * const ) "task_A", configMINIMAL_STACK_SIZE, (void *)&TASKS[0], TASK_A_PRIORITY, xHandleA );
+        TASKS[0].tick_counter = 1;
+        xTaskCreate( task_work, ( const signed char * const ) "task_A", configMINIMAL_STACK_SIZE, (void *)&TASKS[0], TASK_A_PRIORITY, &(TASKS[0].handler));
+        vTaskSuspend((TASKS[0].handler));
     }
     if (name == 'B'){
         printf("CREATING B \n\r");
-        TASKS[0].name = 'B';
-        xTaskCreate( task_work, ( const signed char * const ) "task_B", configMINIMAL_STACK_SIZE, (void *)&TASKS[1], TASK_B_PRIORITY, xHandleB );
+        TASKS[1].name = 'B';
+        TASKS[1].tick_counter = 1;
+        xTaskCreate( task_work, ( const signed char * const ) "task_B", configMINIMAL_STACK_SIZE, (void *)&TASKS[1], TASK_B_PRIORITY, &(TASKS[1].handler) );
+        vTaskSuspend((TASKS[1].handler));
     }
     if (name == 'C'){
         printf("CREATING C \n\r");
-        TASKS[0].name = 'C';
-        xTaskCreate( task_work, ( const signed char * const ) "task_C", configMINIMAL_STACK_SIZE, (void *)&TASKS[2], TASK_C_PRIORITY, xHandleC );
+        TASKS[2].name = 'C';
+        TASKS[2].tick_counter = 1;
+        xTaskCreate( task_work, ( const signed char * const ) "task_C", configMINIMAL_STACK_SIZE, (void *)&TASKS[2], TASK_C_PRIORITY, &(TASKS[2].handler) );
+        vTaskSuspend((TASKS[2].handler));
     }
     if (name == 'D'){
         printf("CREATING D \n\r");
-        TASKS[0].name = 'D';
-        xTaskCreate( task_work, ( const signed char * const ) "task_D", configMINIMAL_STACK_SIZE, (void *)&TASKS[3], TASK_C_PRIORITY, NULL );
+        TASKS[3].name = 'D';
+        TASKS[3].tick_counter = 1;
+        xTaskCreate( task_work, ( const signed char * const ) "task_D", configMINIMAL_STACK_SIZE, (void *)&TASKS[3], TASK_D_PRIORITY, &(TASKS[3].handler) );
+        vTaskSuspend((TASKS[3].handler));
     }
     if (name == 'E'){
         printf("CREATING E \n\r");
-        TASKS[0].name = 'E';
-        xTaskCreate( task_work, ( const signed char * const ) "task_E", configMINIMAL_STACK_SIZE, (void *)&TASKS[4], TASK_C_PRIORITY, NULL );
+        TASKS[4].name = 'E';
+        TASKS[4].tick_counter = 1;
+        xTaskCreate( task_work, ( const signed char * const ) "task_E", configMINIMAL_STACK_SIZE, (void *)&TASKS[4], TASK_E_PRIORITY, &(TASKS[4].handler) );
+        vTaskSuspend((TASKS[4].handler));
     }
     if (name == 'F'){
         printf("CREATING F \n\r");
-        TASKS[0].name = 'F';
-        xTaskCreate( task_work, ( const signed char * const ) "task_F", configMINIMAL_STACK_SIZE, (void *)&TASKS[5], TASK_C_PRIORITY, NULL );
+        TASKS[5].name = 'F';
+        TASKS[5].tick_counter = 1;
+        xTaskCreate( task_work, ( const signed char * const ) "task_F", configMINIMAL_STACK_SIZE, (void *)&TASKS[5], TASK_F_PRIORITY, &(TASKS[5].handler) );
+        vTaskSuspend((TASKS[5].handler));
     }
 	
 }
 
 void TMAN_TaskRegisterAttributes(char name, int period)
 {
-    if (name == "A"){
-        TASK_A_PERIOD = period;
+    if (name == 'A'){
+        TASKS[0].period = period;
     }
     
-    if (name == "B"){
-        TASK_B_PERIOD = period;
+    if (name == 'B'){
+        TASKS[1].period = period;
     }
     
-    if (name == "C"){
-        TASK_C_PERIOD = period;
+    if (name == 'C'){
+        TASKS[2].period = period;
     }
+    
+    if (name == 'D'){
+        TASKS[3].period = period;
+    }
+    
+    if (name == 'E'){
+        TASKS[4].period = period;
+    }
+    
+    if (name == 'F'){
+        TASKS[5].period = period;
+    }
+    
     
 }
 
 void TMAN_TaskWaitPeriod(void *pvParam)
 {
-    /*struct TASK *working_task;
-    working_task =(struct TASK *)pvParam;
-    vTaskDelay();*/
     vTaskSuspend(NULL);
 }
 
@@ -178,34 +194,38 @@ void task_tick_work(void *pvParam)
 {
     printf("TASK TICK WORK\n\r");
     TickType_t xLastWakeTime;
-    const TickType_t xFrequency = TASK_TICK_PERIOD_MS;
+    const TickType_t xFrequency = TASK_TICK_PERIOD;
     xLastWakeTime = xTaskGetTickCount();
     
     for(;;){
-        //rtos_tick = xTaskGetTickCount();
         vTaskDelayUntil( &xLastWakeTime, xFrequency );
         
         TMAN_TICK = TMAN_TICK+1;
-        if (TMAN_TICK % 3 == 0){
-            vTaskResume(xHandleA);
-        }
-        if (TMAN_TICK % 3 == 1){
-            vTaskResume(xHandleB);
-        }
-        if (TMAN_TICK % 3 == 2){
-            vTaskResume(xHandleC);
-        }
-            // ACORAR TAREFA
         printf("RTOS_TICK = (%d)\n\r", xLastWakeTime);
         printf("TMAN_TICK = (%d)\n\r", TMAN_TICK);
+        
+        // ACORAR TAREFA
+        int task_to_resume = 0;
+        for (task_to_resume = 0; task_to_resume<6; task_to_resume++){
+            //printf(" --------- TASK TO RESUME: (%d) \n\r", task_to_resume);
+            if (TASKS[task_to_resume].tick_counter == TASKS[task_to_resume].period){
+                printf(" --------- TASK (%c) TICK_COUNTER: (%d) \n\r",TASKS[task_to_resume].name, TASKS[task_to_resume].tick_counter);
+                vTaskResume((TASKS[task_to_resume].handler));
+                TASKS[task_to_resume].tick_counter =1;
+                break;
+            }
+            else{
+                //printf(" --------- INCREMENT COUNTER \n\r", task_to_resume);
+                TASKS[task_to_resume].tick_counter++;
+            }
+        }
     }
 }
 
 
 void task_work(void *pvParam)
 {
-    
-    printf("TASK WORK\n\r");
+
     struct TASK *working_task;
     working_task =(struct TASK *)pvParam;
     
@@ -231,29 +251,6 @@ void task_work(void *pvParam)
     }
 }
 
-void pvInterfTask(void *pvParam)
-{
-    
-    volatile uint32_t counter1, counter2;
-    float x=100.1;
-            
-    for(;;) {
-        PORTCbits.RC1 = 1;        
-        PrintStr("Interfering task release ...");
-        
-        /* Workload. In this case just spend CPU time ...*/        
-        for(counter1=0; counter1 < INTERF_WORKLOAD; counter1++ )
-            for(counter2=0; counter2 < 0x10200; counter2++ )
-            x=x/3;                
-
-        PrintStr("and termination!\n\r");
-        PORTCbits.RC1 = 0;        
-        
-        //vTaskDelay(INTERF_PERIOD_MS);         
-    }
-}
-
-
 /*
  * Create the demo tasks then start the scheduler.
  */
@@ -267,17 +264,23 @@ int mainSetrLedBlink( void )
 
     __XC_UART = 1; /* Redirect stdin/stdout/stderr to UART1*/
     
-    TMAN_Init();
+    TMAN_Init(100);
     
     printf("AFTER INIT!\n\r");
 
     TMAN_TaskAdd('A');
     TMAN_TaskAdd('B');
     TMAN_TaskAdd('C');
+    TMAN_TaskAdd('D');
+    TMAN_TaskAdd('E');
+    TMAN_TaskAdd('F');
 
-    //TMAN_TaskRegisterAttributes("A", (3000 / portTICK_RATE_MS));
-    //TMAN_TaskRegisterAttributes("B", (2000 / portTICK_RATE_MS));
-    //TMAN_TaskRegisterAttributes("C", (1000 / portTICK_RATE_MS));
+    TMAN_TaskRegisterAttributes('A', 5);
+    TMAN_TaskRegisterAttributes('B', 2);
+    TMAN_TaskRegisterAttributes('C', 3);
+    TMAN_TaskRegisterAttributes('D', 6);
+    TMAN_TaskRegisterAttributes('E', 7);
+    TMAN_TaskRegisterAttributes('F', 3);
     
     vTaskStartScheduler();
     
