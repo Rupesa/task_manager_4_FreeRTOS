998898b51231db62e20c35d43a8aeac9fbe3059b
Rodrigo Santos <56035598+RodrigoSantosgit@users.noreply.github.com>
2022-02-04 16:44:59 +0000

    VERY DONE

--- PIC32MX_ChipKitLedBlink/mainSETRLedBlink.c
+++ PIC32MX_ChipKitLedBlink/mainSETRLedBlink.c
@@ -42,13 +42,7 @@ int TMAN_N_TASKS;    // NUMBER OF TMAN TASKS
 #define INTERF_WORKLOAD          ( 20)
 
 /* Priorities of the demo application tasks (high numb. -> high prio.) */
-#define TASK_A_PRIORITY	 ( tskIDLE_PRIORITY + 1 )
-#define TASK_B_PRIORITY	 ( tskIDLE_PRIORITY + 1 )
-#define TASK_C_PRIORITY  ( tskIDLE_PRIORITY + 1 )
-#define TASK_D_PRIORITY  ( tskIDLE_PRIORITY + 1 )
-#define TASK_E_PRIORITY  ( tskIDLE_PRIORITY + 1 )
-#define TASK_F_PRIORITY  ( tskIDLE_PRIORITY + 1 )
-#define TASK_TICK_PRIORITY ( tskIDLE_PRIORITY + 2 )
+#define TASK_TICK_PRIORITY ( tskIDLE_PRIORITY + 4 )
 
 /* Task Structure */
 struct TASK {
@@ -59,25 +53,27 @@ struct TASK {
    int phase;             // task phase
    int deadline;          // task deadline
    int deadline_misses;   // number of deadline misses
-   int ready;          // task executed on current period
-   int precedence[6];      // task precedence chain
-   int goingToExecute;    // flag to signal that task is going to execute this tick
+   int ready;             // task executed on current period
+   int precedence[5];     // task precedence chain
    TaskHandle_t handler;  // task Handler
 };
 
-struct TASK TASKS[6];
+struct TASK TASKS[6];     // Tasks array
+int task_id;              // id for initialization
 
 /*
- * Prototypes and tasks
+ * Prototypes
  */
 void task_work(void *pvParam);
 void TMAN_Close(void);
 void TMAN_TaskAdd(char name);
-void TMAN_TaskRegisterAttributes(char name, int period, int phase, int deadline, int precedence_constraints[]);
-void TMAN_TaskWaitPeriod(void *pvParam);
+void TMAN_TaskRegisterAttributes(char name, int priority, int period, int phase, int deadline, int precedence_constraints[]);
+void TMAN_TaskWaitPeriod(void);
 void TMAN_TaskStats(void);
 void task_tick_work(void *pvParam);
 void task_manager(void);
+void taskModifyPeriod(char name, int period);
+void taskModifyPhase(char name, int phase);
 
 void TMAN_Init(int TMAN_TICK_PERIOD_VALUE, int N_TASKS)
 {
@@ -86,6 +82,9 @@ void TMAN_Init(int TMAN_TICK_PERIOD_VALUE, int N_TASKS)
     printf("**********************************************\n\r");
     printf("  TMAN - Task Manager framework for FreeRTOS  \n\r");
     printf("**********************************************\n\r");
+    
+    /* ID para Inicialização */
+    task_id = 0;
     
     /* Inicialização do Tick */
     TMAN_TICK = 0;
@@ -102,6 +101,26 @@ void TMAN_Init(int TMAN_TICK_PERIOD_VALUE, int N_TASKS)
 
 }
 
+void taskModifyPeriod(char name, int period){
+    
+    for(int j = 0; j < TMAN_N_TASKS; j++){
+        if (TASKS[j].name == name){
+            TASKS[j].period = period;
+        }
+    }
+    
+}
+
+void taskModifyPhase(char name, int phase){
+    
+    for(int j = 0; j < TMAN_N_TASKS; j++){
+        if (TASKS[j].name == name){
+            TASKS[j].phase = phase;
+        }
+    }
+    
+}
+
 void TMAN_Close(void)
 {
     // exit
@@ -111,120 +130,38 @@ void TMAN_Close(void)
 void TMAN_TaskAdd(char name)
 {
     /* Create the tasks defined within this file. */
-    if (name == 'A'){
-        TASKS[0].name = 'A';
-        TASKS[0].deadline_misses = 0;
-        TASKS[0].ready = 0;
-        TASKS[0].goingToExecute = 0;
-        TASKS[0].activations = 0;
-        xTaskCreate( task_work, ( const signed char * const ) "task_A", configMINIMAL_STACK_SIZE, (void *)&TASKS[0], TASK_A_PRIORITY, &(TASKS[0].handler));
-        vTaskSuspend((TASKS[0].handler));
-    }
-    if (name == 'B'){
-        TASKS[1].name = 'B';
-        TASKS[1].deadline_misses = 0;
-        TASKS[1].ready = 0;
-        TASKS[1].goingToExecute = 0;
-        TASKS[1].activations = 0;
-        xTaskCreate( task_work, ( const signed char * const ) "task_B", configMINIMAL_STACK_SIZE, (void *)&TASKS[1], TASK_B_PRIORITY, &(TASKS[1].handler) );
-        vTaskSuspend((TASKS[1].handler));
-    }
-    if (name == 'C'){
-        TASKS[2].name = 'C';
-        TASKS[2].deadline_misses = 0;
-        TASKS[2].ready = 0;
-        TASKS[2].goingToExecute = 0;
-        TASKS[2].activations = 0;
-        xTaskCreate( task_work, ( const signed char * const ) "task_C", configMINIMAL_STACK_SIZE, (void *)&TASKS[2], TASK_C_PRIORITY, &(TASKS[2].handler) );
-        vTaskSuspend((TASKS[2].handler));
-    }
-    if (name == 'D'){
-        TASKS[3].name = 'D';
-        TASKS[3].deadline_misses = 0;
-        TASKS[3].ready = 0;
-        TASKS[3].goingToExecute = 0;
-        TASKS[3].activations = 0;
-        xTaskCreate( task_work, ( const signed char * const ) "task_D", configMINIMAL_STACK_SIZE, (void *)&TASKS[3], TASK_D_PRIORITY, &(TASKS[3].handler) );
-        vTaskSuspend((TASKS[3].handler));
-    }
-    if (name == 'E'){
-        TASKS[4].name = 'E';
-        TASKS[4].deadline_misses = 0;
-        TASKS[4].ready = 0;
-        TASKS[4].goingToExecute = 0;
-        TASKS[4].activations = 0;
-        xTaskCreate( task_work, ( const signed char * const ) "task_E", configMINIMAL_STACK_SIZE, (void *)&TASKS[4], TASK_E_PRIORITY, &(TASKS[4].handler) );
-        vTaskSuspend((TASKS[4].handler));
-    }
-    if (name == 'F'){
-        TASKS[5].name = 'F';
-        TASKS[5].deadline_misses = 0;
-        TASKS[5].ready = 0;
-        TASKS[5].goingToExecute = 0;
-        TASKS[5].activations = 0;
-        xTaskCreate( task_work, ( const signed char * const ) "task_F", configMINIMAL_STACK_SIZE, (void *)&TASKS[5], TASK_F_PRIORITY, &(TASKS[5].handler) );
-        vTaskSuspend((TASKS[5].handler));
-    }
+    
+    TASKS[task_id].name = name;
+    TASKS[task_id].deadline_misses = 0;
+    TASKS[task_id].ready = 0;
+    TASKS[task_id].activations = 0;
+    char task_name[6] = "task";
+    task_name[4] = name;
+    xTaskCreate( task_work, ( const signed char * const ) task_name, configMINIMAL_STACK_SIZE, (void *)&TASKS[task_id], tskIDLE_PRIORITY, &(TASKS[task_id].handler));
+    vTaskSuspend((TASKS[task_id].handler));
+    
+    task_id++;
+    
 }
 
-void TMAN_TaskRegisterAttributes(char name, int period, int phase, int deadline, int precedence_constraints[])
+void TMAN_TaskRegisterAttributes(char name, int priority, int period, int phase, int deadline, int precedence_constraints[])
 {
-    if (name == 'A'){
-        TASKS[0].period = period;
-        TASKS[0].phase = phase;
-        TASKS[0].deadline = deadline;
-        for (int i = 0; i<6; i++){
-            TASKS[0].precedence[i] = precedence_constraints[i];
+    
+    for(int j = 0; j < TMAN_N_TASKS; j++){
+        if (TASKS[j].name == name){
+            TASKS[j].period = period;
+            TASKS[j].phase = phase;
+            TASKS[j].deadline = deadline;
+            TASKS[j].priority = priority;
+            vTaskPrioritySet( TASKS[j].handler, TASKS[j].priority );
+            for (int i = 0; i<5; i++){
+                TASKS[j].precedence[i] = precedence_constraints[i];
+            }
         }
     }
-    
-    if (name == 'B'){
-        TASKS[1].period = period;
-        TASKS[1].phase = phase;
-        TASKS[1].deadline = deadline;
-        for (int i = 0; i<6; i++){
-            TASKS[1].precedence[i] = precedence_constraints[i];
-        }
-    }
-    
-    if (name == 'C'){
-        TASKS[2].period = period;
-        TASKS[2].phase = phase;
-        TASKS[2].deadline = deadline;
-        for (int i = 0; i<6; i++){
-            TASKS[2].precedence[i] = precedence_constraints[i];
-        }
-    }
-    
-    if (name == 'D'){
-        TASKS[3].period = period;
-        TASKS[3].phase = phase;
-        TASKS[3].deadline = deadline;
-        for (int i = 0; i<6; i++){
-            TASKS[3].precedence[i] = precedence_constraints[i];
-        }
-    }
-    
-    if (name == 'E'){
-        TASKS[4].period = period;
-        TASKS[4].phase = phase;
-        TASKS[4].deadline = deadline;
-        for (int i = 0; i<6; i++){
-            TASKS[4].precedence[i] = precedence_constraints[i];
-        }
-    }
-    
-    if (name == 'F'){
-        TASKS[5].period = period;
-        TASKS[5].phase = phase;
-        TASKS[5].deadline = deadline;
-        for (int i = 0; i<6; i++){
-            TASKS[5].precedence[i] = precedence_constraints[i];
-        }
-    } 
 }
 
-void TMAN_TaskWaitPeriod(void *pvParam)
+void TMAN_TaskWaitPeriod(void)
 {
     vTaskSuspend(NULL);
 }
@@ -243,7 +180,6 @@ void task_manager(void){
     
     int task_to_resume = 0;
         for (task_to_resume = 0; task_to_resume<TMAN_N_TASKS; task_to_resume++){
-            //printf(" --------- TASK TO RESUME: (%d) \n\r", task_to_resume);
             if ((TMAN_TICK % TASKS[task_to_resume].period) == TASKS[task_to_resume].phase) { 
                 TASKS[task_to_resume].ready += 1;
                 TASKS[task_to_resume].activations += 1;
@@ -253,12 +189,9 @@ void task_manager(void){
         for (int task = 0; task < TMAN_N_TASKS; task++){
             int dont_executable = 0;
             if (TASKS[task].ready > 0) {
-                    for (int i = 0; i<6; i++){
-                        //printf(" -- precedencia id: (%d) \n\r", TASKS[task].precedence[i]);
-                        if (TASKS[task].precedence[i]!= -1){
-                            //printf(" --------- Precedence exists \n\r");
-                            if (TASKS[TASKS[task].precedence[i]].ready > 0){
-                                //printf(" --------- INCREMENT EXECUTABLE \n\r");
+                    for (int i = 0; i<5; i++){
+                        if (TASKS[task].precedence[i]!= -1){      
+                            if (TASKS[TASKS[task].precedence[i]].ready > 0){ 
                                 dont_executable++;
                             }
                         }
@@ -267,10 +200,7 @@ void task_manager(void){
                         vTaskResume((TASKS[task].handler));
                     }
             }
-            /*
-            printf("Resto de divisao: %d\n\r", (TMAN_TICK % TASKS[task_to_resume].period))
-            printf("Deadline -1 (1): %d\n\r", (TASKS[task].deadline - 1))
-            printf("Resto de divisao: %d\n\r", (TMAN_TICK % TASKS[task_to_resume].period)*/
+            
             if (((TMAN_TICK % TASKS[task].period) == (TASKS[task].deadline - 1)) &&  (TASKS[task].ready > 0)){
                 printf(" --------- TASK (%c) DEADLINE MISS! \n\r", TASKS[task].name);
                 TASKS[task].deadline_misses += 1;
@@ -306,19 +236,21 @@ void task_work(void *pvParam)
     
     int i;
     int j;
-    int IMAXCOUNT = 999999;
-    int JMAXCOUNT = 99999999;
+    int IMAXCOUNT = 9999;
+    //int JMAXCOUNT = 99999999;
     
     for(;;){
-        printf("%c, %d \n\r", working_task->name, TMAN_TICK);
                 
         for(i=0; i<IMAXCOUNT; i++){
             /*for(j=0; j<JMAXCOUNT; j++){
                 
             }*/
         }
+        
+        printf("%c, %d \n\r", working_task->name, TMAN_TICK);
+        
         working_task->ready -= 1;
-        TMAN_TaskWaitPeriod((void *)&working_task);
+        TMAN_TaskWaitPeriod();
     }
 }
 
@@ -344,19 +276,19 @@ int mainSetrLedBlink( void )
     TMAN_TaskAdd('E');
     TMAN_TaskAdd('F');
     
-    int a_precedences[] = {-1,-1,-1,-1,-1,-1}; 
-    int b_precedences[] = {2,-1,-1,-1,-1,-1}; 
-    int c_precedences[] = {5,-1,-1,-1,-1,-1}; 
-    int d_precedences[] = {-1,-1,-1,-1,-1,-1};
-    int e_precedences[] = {-1,-1,-1,-1,-1,-1};
-    int f_precedences[] = {-1,-1,-1,-1,-1,-1}; 
+    int a_precedences[] = {-1,-1,-1,-1,-1}; 
+    int b_precedences[] = {-1,-1,-1,-1,-1}; 
+    int c_precedences[] = {-1,-1,-1,-1,-1}; 
+    int d_precedences[] = {-1,-1,-1,-1,-1};
+    int e_precedences[] = {-1,-1,-1,-1,-1};
+    int f_precedences[] = {-1,-1,-1,-1,-1}; 
 
-    TMAN_TaskRegisterAttributes('A', 5, 2, 5, a_precedences);
-    TMAN_TaskRegisterAttributes('B', 2, 0, 2, b_precedences);
-    TMAN_TaskRegisterAttributes('C', 3, 0, 3, c_precedences);
-    TMAN_TaskRegisterAttributes('D', 6, 1, 6, d_precedences);
-    TMAN_TaskRegisterAttributes('E', 7, 1, 7, e_precedences);
-    TMAN_TaskRegisterAttributes('F', 3, 0, 2, f_precedences);
+    TMAN_TaskRegisterAttributes('A', tskIDLE_PRIORITY + 0, 5, 2, 5, a_precedences);
+    TMAN_TaskRegisterAttributes('B', tskIDLE_PRIORITY + 1, 2, 0, 2, b_precedences);
+    TMAN_TaskRegisterAttributes('C', tskIDLE_PRIORITY + 2, 3, 0, 3, c_precedences);
+    TMAN_TaskRegisterAttributes('D', tskIDLE_PRIORITY + 3, 6, 1, 6, d_precedences);
+    TMAN_TaskRegisterAttributes('E', tskIDLE_PRIORITY + 3, 7, 1, 7, e_precedences);
+    TMAN_TaskRegisterAttributes('F', tskIDLE_PRIORITY + 3, 3, 0, 2, f_precedences);
     
     vTaskStartScheduler();
     
